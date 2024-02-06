8f8e965f953690a1db1d7a6ca69a8732ae1e606b
dkar1mov03 <dkarimov049@gmail.com>
2024-02-06 12:27:26 +0500

    Add : Enum for Category

--- MyMoneyManager.Domain/Entities/Transaction.cs
+++ MyMoneyManager.Domain/Entities/Transaction.cs
@@ -7,6 +7,8 @@ public class Transaction : Auditable
 {
     public long WalletId { get; set; }
     public Wallet Wallet { get; set; }
+    public long CategoryId { get; set; }
+    public Category Category { get; set; }
     public decimal Balance { get; set; }
     public string Description { get; set; }
     public TransactionType TransactionType { get; set; }
